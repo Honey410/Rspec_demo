f256731df4a256f5c544f8d8e348b5588e770df0
Honey410 <honeyjhamnani410@gmail.com>
2021-09-02 11:26:21 +0530

    Custom_matcher

--- spec/custom_matcher.rb
+++ spec/custom_matcher.rb
@@ -1,10 +1,16 @@
 module CustomMatcher
     class OurCustomMatcher
+        def initialize(target)
+            @target = target
+        end
         def matches?(value)
-            true
+            value ==@target
+        end
+        def failure_message
+            "Our matcher failed to match"
         end
     end
-    def self.custom_matcher 
-        OurCustomMatcher.new
+    def self.custom_matcher(target)
+        OurCustomMatcher.new(target)
     end
 end
